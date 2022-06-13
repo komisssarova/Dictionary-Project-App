5b6ec7c8aed1a950c96565da513219689ad2e5bd
komisssarova <komisssarova.uk@gmail.com>
2022-06-13 17:36:42 +0100

    added form, search engine

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Dictionary App</h1>
+      <div className="container">
+        <header className="App-header"></header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>
+            Coded by{" "}
+            <a href="https://www.linkedin.com/in/kateryna-komisarova/">
+              Kateryna Komisarova
+            </a>
+          </small>
+        </footer>
+      </div>
     </div>
   );
 }
-
